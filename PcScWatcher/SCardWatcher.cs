88de4a3095f71b47c561c8a3b32a2aa8e7968de7
Ryu Youngseok <ryu.youngseok0512@mediasol.kr>
2020-08-07 17:11:04 +0900

    초기 커밋.

---
+++ PcScWatcher/SCardWatcher.cs
@@ -0,0 +1,124 @@
+﻿using PCSC;
+using PCSC.Iso7816;
+using PCSC.Monitoring;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PcScWatcher
+{
+    public class SCardWatcher : IDisposable
+    {
+        private ISCardMonitor monitor;
+
+        public Action<string> LogAction;
+
+        public delegate void SCardUidReceivedHandler(object sender, CardUidReceivedEventArgs e);
+        public event SCardUidReceivedHandler UidReceived;
+        public bool IsInitialized
+        {
+            get
+            {
+                return monitor != null;
+            }
+        }
+
+        public string ConnectedDeviceName
+        {
+            get
+            {
+                return monitor == null ? null : monitor.ReaderNames[0];
+            }
+        }
+
+        public void Initialize()
+        {
+            using (var context = ContextFactory.Instance.Establish(SCardScope.System))
+            {
+                var readerNames = context.GetReaders();
+                monitor = MonitorFactory.Instance.Create(SCardScope.System);
+
+                monitor.CardInserted += Monitor_CardInserted;
+                monitor.Start(readerNames[0]);
+
+                LogAction?.Invoke($"SCardWatcher Initialized : Connected to {readerNames[0]}");
+            }
+        }
+
+        public void Initialize(string explicitName)
+        {
+            monitor = MonitorFactory.Instance.Create(SCardScope.System);
+
+            monitor.CardInserted += Monitor_CardInserted;
+            monitor.Start(explicitName);
+
+            LogAction?.Invoke($"SCardWatcher Initialized : Connected to {explicitName}");
+        }
+
+        private void Monitor_CardInserted(object sender, CardStatusEventArgs e)
+        {
+            var contextFactory = ContextFactory.Instance;
+
+            try
+            {
+                using (var ctx = contextFactory.Establish(SCardScope.System))
+                {
+                    using (var isoReader = new IsoReader(ctx, monitor.ReaderNames[0], SCardShareMode.Shared, SCardProtocol.Any, false))
+                    {
+
+                        var apdu = new CommandApdu(IsoCase.Case2Short, isoReader.ActiveProtocol)
+                        {
+                            CLA = 0xFF,
+                            Instruction = (InstructionCode)0xCA,
+                            P1 = 0x00, 
+                            P2 = 0x00, 
+                            Le = 0x00 
+                        };
+
+                        var response = isoReader.Transmit(apdu);
+                        var data = response.GetData();
+                        UidReceived(this, new CardUidReceivedEventArgs(data));
+                        LogAction?.Invoke("SCardWatcher Received : " + BitConverter.ToString(data));
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                LogAction?.Invoke("SCardWatcher Error - " + ex);
+            }
+        }
+
+        #region IDisposable Support
+        private bool disposedValue = false; 
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    monitor.Dispose();
+                }
+                disposedValue = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+        #endregion
+    }
+
+    public class CardUidReceivedEventArgs : EventArgs
+    {
+        public byte[] ReceivedData { get; set; }
+
+        public CardUidReceivedEventArgs(byte[] data)
+        {
+            ReceivedData = data;
+        }
+    }
+}
---
+++ PcScWatcherTester/MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using PcScWatcher;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PcScWatcherTester
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private SCardWatcher watcher = null;
+        public MainWindow()
+        {
+            InitializeComponent();
+            Title = "Not Connected";
+            watcher = new SCardWatcher();
+            watcher.UidReceived += Watcher_UidReceived;
+            watcher.LogAction += new Action<string>((s) =>
+            {
+                Dispatcher.Invoke(() =>
+                {
+                    tb.Text += $"{DateTime.Now} {s} \n";
+                });
+            });
+            try
+            {
+                watcher.Initialize();
+                if (watcher.IsInitialized)
+                {
+                    Title = watcher.ConnectedDeviceName;
+                }
+            }
+            catch(Exception e)
+            {
+                recvdValue.Text = e.Message;
+            }
+          
+           
+        }
+
+        private void Watcher_UidReceived(object sender, CardUidReceivedEventArgs e)
+        {
+            Dispatcher.Invoke(() =>
+            {
+                recvdValue.Text = BitConverter.ToString(e.ReceivedData);
+            });
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            Dispatcher.Invoke(() =>
+            {
+                tb.ScrollToEnd();
+            });
+        }
+    }
+}
